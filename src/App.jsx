87ba1459f2e3fc35088663b4ca157b693db488d8
MostafaMaged00 <185123397+MostafaMaged00@users.noreply.github.com>
2025-01-23 11:19:00 -0500

    Add navbar and navbar css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div>
+    <>
+       <NavBar/>
       <section>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
           Voluptate provident fuga nihil sapiente, dolorem animi ex id ipsum minus pariatur 
           obcaecati veritatis voluptates sequi error dolorum culpa architecto recusandae tempore!</p>
       </section>
-    </div>
+     
+    </>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './css/Navbar.css';
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className="navbar">
+      <div className="logo">
+        <h1>REACTO</h1>
+      </div>
+      <ul className="nav-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
   )
 }
 
