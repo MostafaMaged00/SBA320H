3a3a07023835adc067764c0a2fd43b85adef2e5a
MostafaMaged00 <185123397+MostafaMaged00@users.noreply.github.com>
2025-01-25 11:39:12 -0500

    connect to api and render onpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Axios } from 'axios'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'; 
 import NavBar from './components/NavBar'
 import { Route,Routes } from 'react-router-dom'
 import Home from './pages/Home'
@@ -7,18 +7,44 @@ import About from './pages/About'
 import Service from './pages/Service'
 import Contact from './pages/contact'
 import Search from './components/Search'
+
 import './App.css';
 
 function App() {
 
-  const BASE_URL = 'https://jsonplaceholder.typicode.com';
+  const [posts,setPosts] = useState([]);
 
-  fetch(`${BASE_URL}/posts`)
-  .then((res)=> res.json())
-  .then((data)=>{
-    console.log(data)
-  })
+  const URL = 'https://jsonplaceholder.typicode.com/posts';
+  //const URL = `https://api.thecatapi.com/v1/images/search?api_key='live_h5xF1HcRsXnJjvhPwKIuZKUhlB0M9F1MyGB322qCciwnslVWldVlHBh5nBXaFAHj`;
 
+  // fetch(`${BASE_URL}/posts`)
+  // .then((res)=> res.json())
+  // .then((data)=>{
+  //   console.log(data)
+  // })
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+ 
+        // Fetch the posts data from JSONPlaceholder API
+        const response = await fetch(URL);
+        
+     
+        const data = await response.json();
+        //console.log(data[0].title)
+        setPosts(data);
+
+     
+    };
+
+    fetchPosts();
+  }, []); // Empty dependency array en
+
+  const today = new Date();
+  const formattedDate = today.toLocaleDateString(); 
+
+ 
 
   return (
     <>
@@ -31,6 +57,22 @@ function App() {
        </Routes>
        <div className='search-container'>
         <Search/>
+
+       </div>
+       <div className='App'>
+       <div>
+      <h1>Hot Jobs</h1>
+      <ul>
+      {posts.map(post => (
+          <li key={post.id}>
+            <h4>Job Title : {post.title.toUpperCase()}</h4>
+            <p><h4>Job Description</h4> {post.body}</p>
+            <p>Opening : {post.userId}</p>
+            <p>Date posted: {formattedDate}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
        </div>
 
      
