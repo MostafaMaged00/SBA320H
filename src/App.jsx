277a18ad43ecbc7e4eadd28ff3578733a0aab0a4
MostafaMaged00 <185123397+MostafaMaged00@users.noreply.github.com>
2025-01-25 09:43:34 -0500

    Install and add axios lib to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Axios } from 'axios'
 import NavBar from './components/NavBar'
 import { Route,Routes } from 'react-router-dom'
 import Home from './pages/Home'
@@ -9,6 +10,16 @@ import Search from './components/Search'
 import './App.css';
 
 function App() {
+
+  const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+  fetch(`${BASE_URL}/posts`)
+  .then((res)=> res.json())
+  .then((data)=>{
+    console.log(data)
+  })
+
+
   return (
     <>
        <NavBar/>
